f1f9d6389fac1c3e138cbc626a9e77c7af844cb2
sara-rioseco <sara.rioseco@gmail.com>
2025-06-10 22:34:40 -0400

    fix junit syntax

--- taskmaster/src/test/java/com/equipo/taskmaster/AppTest.java
+++ taskmaster/src/test/java/com/equipo/taskmaster/AppTest.java
@@ -1,6 +1,7 @@
+
 package com.equipo.taskmaster;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class AppTest {
     @Test
